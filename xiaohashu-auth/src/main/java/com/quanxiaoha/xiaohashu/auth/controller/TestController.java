4577ac027361bbad4b0631cfabb80f2f1c1cdb2f
SilentJ1 <2369837839@qq.com>
2025-08-13 15:44:26 +0800

    添加了 framework 基础设施模块,在该模块中添加了 xiaoha-common通用模块

---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.quanxiaoha.xiaohashu.auth.controller;
+
+import com.quanxiaoha.framework.common.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public Response<String> test() {
+        return Response.success("Hello, 犬小哈专栏");
+    }
+}
+
