a1cb5c0438bb3cc6f8374873c7d6d4a618270d9e
SilentJ1 <2369837839@qq.com>
2025-08-18 16:36:45 +0800

    update:网关整合SaToken实现鉴权工具类

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/constant/RedisKeyConstants.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/constant/RedisKeyConstants.java
@@ -3,7 +3,7 @@ package com.quanxiaoha.xiaohashu.auth.constant;
 public class RedisKeyConstants {
 
     /**
-     * 验证码前缀
+     * 验证码 KEY 前缀
      */
     private static final String VERIFICATION_CODE_KEY_PREFIX = "verification_code:";
 
@@ -11,6 +11,16 @@ public class RedisKeyConstants {
      * 小哈书全局 ID 生成器 KEY
      */
     public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu.id.generator";
+
+    /**
+     * 用户角色数据 KEY 前缀
+     */
+    private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
+
+    /**
+     * 角色对应的权限集合 KEY 前缀
+     */
+    private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
 
     /**
      * 构建验证码 KEY
@@ -22,32 +32,20 @@ public class RedisKeyConstants {
     }
 
     /**
-     * 用户角色数据 KEY 前缀
-     */
-    private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
-
-
-    /**
-     * 构建用户-角色 Key
-     * @param phone
+     * 用户对应的角色集合 KEY
+     * @param userId
      * @return
      */
-    public static String buildUserRoleKey(String phone) {
-        return USER_ROLES_KEY_PREFIX + phone;
+    public static String buildUserRoleKey(Long userId) {
+        return USER_ROLES_KEY_PREFIX + userId;
     }
 
     /**
-     * 角色对应的权限集合 KEY 前缀
-     */
-    private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
-
-
-    /**
      * 构建角色对应的权限集合 KEY
-     * @param roleId
+     * @param roleKey
      * @return
      */
-    public static String buildRolePermissionsKey(Long roleId) {
-        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
+    public static String buildRolePermissionsKey(String roleKey) {
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleKey;
     }
 }
